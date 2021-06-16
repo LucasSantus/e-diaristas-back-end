f793e928ee77ed2cff85a6de34498a1efb323abe
Lucas Santus <leos9877@gmail.com>
2021-06-16 10:46:40 -0300

    adicionando Bootstrap ao projeto

--- project/settings.py
+++ project/settings.py
@@ -106,6 +106,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#DIZENDO ONDE ESTÁ LOCALIZADA A PASTA STATIC
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
