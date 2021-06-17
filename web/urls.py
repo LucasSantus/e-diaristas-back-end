d0ee7d845040f6f381e2f1be25be2458291cdee3
Lucas Santus <leos9877@gmail.com>
2021-06-17 09:43:56 -0300

    implementando funções de editar e remover

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import cadastrar_diarista, listar_diaristas
+from .views import cadastrar_diarista, listar_diaristas, alterar_diarista, remover_diarista
 
 urlpatterns = [
-    path('cadastrar-diarista', cadastrar_diarista, name='cadastrar_diarista'),
-    path('listar-diaristas', listar_diaristas, name='listar_diaristas'),
+    path('cadastrar-diarista/', cadastrar_diarista, name='cadastrar_diarista'),
+    path('listar-diaristas/', listar_diaristas, name='listar_diaristas'),
+    path('alterar-diarista/<int:id_diarista>', alterar_diarista, name='alterar_diarista'),
+    path('remover-diarista/<int:id_diarista>', remover_diarista, name='remover_diarista'),
+
 ]
--- web/views.py
+++ web/views.py
@@ -1,21 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import DiaristaForm
 from .models import Diarista
+from django.shortcuts import get_object_or_404
 
 def home(request):
     return render(request, 'index.html')
 
 def cadastrar_diarista(request):
     if request.method == 'POST':
-        form_diarista = DiaristaForm(request.POST, request.FILES)
-        if form_diarista.is_valid():
-            form_diarista.save()
+        form = DiaristaForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_diaristas')
 
     else:
-        form_diarista = DiaristaForm()
+        form = DiaristaForm()
 
     context = {
-        'form': form_diarista,
+        'form': form,
     }
 
     return render(request, 'web/cadastrar_diarista.html', context)
@@ -30,23 +32,23 @@ def listar_diaristas(request):
     return render(request, 'web/listar_diaristas.html', context)
 
 def alterar_diarista(request, id_diarista):
-    diarista = get_object_or_404(Post, id=id_diarista)
-    form = DiaristaForm(request.POST or None, instance=diarista)
-    if form.is_valid():
-        form.save()
-        return redirect('listar-diarista', pk=post.pk)
-
+    diarista = Diarista.objects.get(id=id_diarista)
+    if request.method == "POST":
+        form = DiaristaForm(request.POST or None, request.FILES, instance=diarista)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_diaristas')
+    else:
+        form = DiaristaForm(instance=diarista)
+        
     context = {
-        'form': diarista,
+        'form': form,
     }
 
-    return render(request, 'web/listar_diaristas.html', context)
+    return render(request, 'web/cadastrar_diarista.html', context)
 
-def remover_diarista(request):
-    diaristas = Diarista.objects.all()
-    
-    context = {
-        'diaristas': diaristas,
-    }
+def remover_diarista(request, id_diarista):
+    diarista = Diarista.objects.get(id=id_diarista)
+    diarista.delete()
 
-    return render(request, 'web/listar_diaristas.html', context)
+    return redirect('listar_diaristas')
